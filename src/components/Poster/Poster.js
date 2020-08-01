015d6297aced3b188e0d4668d3a9347fb4838b5e
astanusic <adrienpro.stanusic@gmail.com>
2020-08-01 19:44:08 +0200

    feat: :sparkles: added trailers fro myoutube on poster click, but it suxx honestly

--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Poster({ imgSrc, alt, className }) {
-  return <img src={imgSrc} alt={alt} className={className} />;
+function Poster({ imgSrc, alt, className, onClick }) {
+  return <img onClick={onClick} src={imgSrc} alt={alt} className={className} />;
 }
 
 export default Poster;
--- src/layout/row/Row.js
+++ src/layout/row/Row.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { TmdbInstance, TmdbImgBaseUrl } from "../../utils/axios";
 import Poster from "../../components/Poster/Poster";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 import "./Row.css";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -15,18 +18,38 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
 
-  console.table(movies);
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
+  console.log(trailerUrl);
 
   return (
     <section className="row">
       <h2>{title}</h2>
 
       <div className="row__posters">
-        {/* multiple row_posters */}
-
         {movies.map((movie) => (
           <Poster
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             imgSrc={`${TmdbImgBaseUrl}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -35,6 +58,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </section>
   );
 }
