b6323b39362ccf57740c3815a9d7d3e1cd2a3aae
astanusic <adrienpro.stanusic@gmail.com>
2020-08-01 14:18:11 +0200

    feat: :sparkles: Added requests files in utils folder to pass featchUrls as props in a futur

---
+++ src/utils/request.js
@@ -0,0 +1,14 @@
+const API_KEY = "16f931d6938e74b558cb01e53faf1064";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=fr-FR`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=fr-FR`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
