ea2d1a04ec6b4f798f1706f481166c03d573be12
astanusic <adrienpro.stanusic@gmail.com>
2020-08-02 23:04:15 +0200

    feat: :sparkles: play button from hero now playing trailer

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,10 +2,16 @@ import React, { useState, useEffect } from "react";
 import DropDown from "../../components/Drop-down/Drop-down";
 import { TmdbInstance, TmdbImgBaseUrl } from "../../utils/axios";
 import { truncate } from "../../utils/truncate";
+import YouTube from "react-youtube";
+import getYouTubeID from "get-youtube-id";
+import movieTrailer from "movie-trailer";
+
 import "./Hero.css";
 
 function Hero({ fetchUrl, hasDropDown, handleItemClick }) {
   const [movie, setMovie] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+  const [hasBeenClicked, setHasBeenClicked] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
@@ -19,6 +25,29 @@ function Hero({ fetchUrl, hasDropDown, handleItemClick }) {
     }
     fetchData();
   }, []);
+
+  const opts = {
+    height: "600",
+    width: "60%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    setHasBeenClicked(!hasBeenClicked);
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(`${movie?.title || movie?.name}`)
+        .then((url) => {
+          const id = getYouTubeID(url);
+          setTrailerUrl(id);
+          console.log(id);
+        })
+        .catch((err) => console.log(err));
+    }
+  };
 
   return (
     <header
@@ -34,16 +63,27 @@ function Hero({ fetchUrl, hasDropDown, handleItemClick }) {
       {hasDropDown && (
         <DropDown title="Movies" handleItemClick={handleItemClick} />
       )}
+
       <div className="hero__contents">
         <h1 className="hero__title">
           {movie?.title || movie?.name || movie?.original_name}
         </h1>
+
         <div className="hero__buttons">
-          <button className="hero__button">Play</button>
+          <button className="hero__button" onClick={() => handleClick(movie)}>
+            Play
+          </button>
           <button className="hero__button">Ma Liste</button>
         </div>
 
-        <h1 className="hero__description">{truncate(movie?.overview, 150)}</h1>
+        <h1 className="hero__description">{truncate(movie?.overview, 250)}</h1>
+        {hasBeenClicked && (
+          <YouTube
+            className="youtube__player"
+            videoId={trailerUrl}
+            opts={opts}
+          />
+        )}
       </div>
 
       <div className="hero--fadeBottom" />
